516febfaada34a275ba52853a3aab82fd21bfee6
lunastra <lantianhaian@hotmail.com>
2025-10-07 22:51:11 +0800

    feat(config): 更新Nacos配置和Dubbo过滤器路径
    - 添加了Nacos配置命名空间支持- 更新了Dubbo消费者和提供者过滤器的包路径
    - 启用了sa-token-alone-redis依赖- 配置了GraalVM原生镜像构建插件
    - 添加了Dubbo AOT处理插件
    - 在TestController中添加线程打印调试信息

--- quantum-web/quantum-web-user/src/main/java/com/lunarstra/quantum/controller/TestController.java
+++ quantum-web/quantum-web-user/src/main/java/com/lunarstra/quantum/controller/TestController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/hello")
     public String hello() {
+        System.out.println(Thread.currentThread());
         return "Hello World!";
     }
 }
