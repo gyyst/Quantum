99f84c261d07671d4274e507c377e5e8095ba04f
lunastra <lantianhaian@hotmail.com>
2025-10-12 20:59:06 +0800

    feat(bot): 初始化量子用户机器人服务项目- 删除默认的 Main.java 示例文件- 配置 Maven项目依赖，引入 simbot、onebot 和 ktor 客户端 - 添加 Spring Boot 启动类 QuantumUserBotService5009- 启用 Dubbo、Simbot 和服务发现功能- 设置 JDK 25 编译目标版本

---
+++ quantum-web/quantum-web-bot/src/main/java/com/lunarstra/quantum/QuantumUserBotService5009.java
@@ -0,0 +1,21 @@
+package com.lunarstra.quantum;
+
+import love.forte.simbot.spring.EnableSimbot;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 主类（项目启动入口）
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableSimbot
+@EnableDubbo
+public class QuantumUserBotService5009 {
+    public static void main(String[] args) {
+        SpringApplication.run(QuantumUserBotService5009.class, args);
+    }
+
+}
